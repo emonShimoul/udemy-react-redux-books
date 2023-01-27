f70572b9f354789a956fcbb5786d8c22a9f1e14c
emonShimoul <emonshimoul@gmail.com>
2023-01-27 23:33:21 +0600

    implement context api

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 const BookList = ({ books, onDelete, onEdit }) => {
+    const { count, incrementCount } = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return <BookShow onDelete={onDelete} key={book.id} book={book} onEdit={onEdit}></BookShow>
     })
 
     return (
         <div className='book-list'>
+            {count}
+            <button onClick={incrementCount}>Click</button>
             {renderedBooks}
         </div>
     );
